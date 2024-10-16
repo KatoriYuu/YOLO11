4a979143232e7593efa54f72232a190babb70223
Henry Bui <henrybui2812@gmail.com>
2024-10-16 22:51:02 +1100

    Support detection in regions and parsing config from json files

--- agent.py
+++ agent.py
@@ -1,20 +1,23 @@
 import fcntl
 import os
 import sys
+
 import SystemEvent
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
-lock_file = file_dir + '/lockfile.lck'
+lock_file = file_dir + "/lockfile.lck"
 print(lock_file)
+
+
 class Locker:
-    def __enter__ (self):
-        os.makedirs(os.path.dirname(lock_file), exist_ok=True)
-        self.fp = open(lock_file)
+    def __enter__(self):
+        self.fp = open(lock_file, "w")
         fcntl.flock(self.fp.fileno(), fcntl.LOCK_EX)
 
-    def __exit__ (self, _type, value, tb):
+    def __exit__(self, _type, value, tb):
         fcntl.flock(self.fp.fileno(), fcntl.LOCK_UN)
         self.fp.close()
+
 
 def main():
     argc = len(sys.argv)
@@ -22,15 +25,16 @@ def main():
         print("agent.py: Incorrect arguments")
         exit(-1)
     with Locker():
-        cam = 'cam' + sys.argv[1]
-        agent = 'agent' + sys.argv[1]
-        invocation = SystemEvent.SystemEvent('invoke')
-        event_cam = SystemEvent.SystemEvent(cam)
+        target = sys.argv[1]
+        agent = "agent-" + target
+        invocation = SystemEvent.SystemEvent("invoke")
+        event_target = SystemEvent.SystemEvent(target)
         event_agent = SystemEvent.SystemEvent(agent)
-        event_cam.set()
+        event_target.set()
         invocation.set()
-        event_agent.wait()
+        event_agent.wait()  # timeout here
         event_agent.clear()
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,81 +1,107 @@
-import sys
+import json
 import os
 import threading
 from threading import Thread
+
+import SystemEvent
 import torch
-import cv2
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 from torch.cuda import device
 from ultralytics import YOLO
-import SystemEvent
 
 lock = threading.Lock()
 
 ai_dir = os.path.dirname(os.path.abspath(__file__))
-file_out_dir = ai_dir + '/output/'
-cam_list = ai_dir + '/cam_list.txt'
+file_out_dir = ai_dir + "/output/"
+json_file_name = ai_dir + "/config.json"
 
 classes = [1, 2, 3, 5, 7]
-device = '0'
 conf = 0.2
 if torch.backends.mps.is_available():
-    device = 'mps'
+    device = "mps"
 elif torch.cuda.is_available():
-    device = 'cuda:0'
-
-def cam_to_source(cam):
-    if not os.path.isfile(cam_list):
-        print('main.py: cam_list file not found')
-        return ''
-    with open(cam_list, 'r') as file:
-        for line in file:
-            args = line.split()
-            if len(args) != 2:
-                print("main.py: cam_list format error")
-                return ''
-            if args[0] == cam:
-                return 'http://admin:abcd123456789@' + args[1] + '/Streaming/channels/1/picture'
-        print('main.py: ' + cam +' not specified in cam_list')
-        return ''
-    return ''
+    device = "cuda:0"
 
 
-def predict(cam, file_out, classes = [], conf = conf, device = device):
-    source = cam_to_source(cam)
-    source = ai_dir + '/c.jpeg'
-    if source == '':
-        print('main.py: ' + cam + ' source not found')
+def json_parse(target):
+    source = ""
+    zone = None
+    with open(json_file_name) as json_file:
+        json_obj = json.load(json_file)
+        if target in json_obj:
+            obj = json_obj[target]
+            if obj["type"] == "cam":
+                username = obj["username"]
+                password = obj["password"]
+                ip = obj["ip"]
+                source = (
+                    "http://"
+                    + username
+                    + ":"
+                    + password
+                    + "@"
+                    + ip
+                    + "/Streaming/channels/1/picture"
+                )
+                zone = obj["zone"]
+            elif obj["type"] == "image":
+                source = obj["path"]
+                zone = obj["zone"]
+    return source, zone
+
+
+def predict(cam, classes=[], conf=conf, device="cpu"):
+    source, zone = json_parse(cam)
+    if source == "":
+        print("main.py: " + cam + " source not found")
+        return
+    file_out = os.path.basename(source + ".txt")
     model = YOLO(ai_dir + "/models/yolo11x.pt")
     counter = dict()
-    predictions = model.predict(source = source, classes = classes, conf = conf, device = device)
+    predictions = model.predict(
+        source=source, classes=classes, conf=conf, device=device
+    )
     for result in predictions:
+        img_shape = result.orig_shape
+        if zone is None:
+            zone = [(0, 0), (0, img_shape[1]), img_shape, (img_shape[0], 0)]
+        polygon = Polygon(zone)
+        result.show()
         for box in result.boxes:
             cls = int(box.cls[0])
             class_name = model.names[cls]
             if class_name not in counter:
                 counter[class_name] = 0
-            counter[class_name] += 1
+            centroid = Point(
+                (float(box.xyxyn[0][2]) + float(box.xyxyn[0][0])) / 2 * img_shape[0],
+                (float(box.xyxyn[0][3]) + float(box.xyxyn[0][1])) / 2 * img_shape[1],
+            )
+            if polygon.contains(centroid):
+                counter[class_name] += 1
     with lock:
         file_out_path = file_out_dir + file_out
         os.makedirs(os.path.dirname(file_out_path), exist_ok=True)
-        with open(file_out_path, 'w') as file:
+        with open(file_out_path, "w") as file:
             for key in counter:
                 print(key, counter[key], file=file)
 
 
 def main():
     while True:
-        invocation = SystemEvent.SystemEvent('invoke')
+        invocation = SystemEvent.SystemEvent("invoke")
         invocation.wait()
         invocation.clear()
-        for i in range(1, 9):
-            cam = 'cam' + str(i)
-            agent = 'agent' + str(i)
-            event_cam = SystemEvent.SystemEvent(cam)
-            event_agent = SystemEvent.SystemEvent(agent)
-            if event_cam.isSet():
-                Thread(target=predict, args=(cam, cam + '.txt', classes, conf)).start()
-                event_cam.clear()
-                event_agent.set()
+        with open(json_file_name) as json_file:
+            json_obj = json.load(json_file)
+            for obj in json_obj:
+                event_target = SystemEvent.SystemEvent(obj)
+                event_agent = SystemEvent.SystemEvent("agent-" + obj)
+                if event_target.isSet():
+                    Thread(target=predict, args=(obj, classes, conf)).start()
+                    event_target.clear()
+                    event_agent.set()
+
 
 if __name__ == "__main__":
     main()
