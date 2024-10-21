400a133490f729a4debbc201e1212958c91aee09
Henry Bui <henrybui2812@gmail.com>
2024-10-21 14:00:26 +1100

    Use normalised coordinates instead of actual shape-based coordinates for points; Change output file naming

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ json_file_name = ai_dir + "/config.json"
 classes = [1, 2, 3, 5, 7]
 conf = 0.2
 
-
 if torch.backends.mps.is_available():
     device = "mps"
 elif torch.cuda.is_available():
@@ -53,21 +52,20 @@ def json_parse(target):
     return source, zone
 
 
-def predict(cam, classes=[], conf=conf, device="cpu"):
-    source, zone = json_parse(cam)
+def predict(target, classes=[], conf=conf, device="cpu"):
+    source, zone = json_parse(target)
     if source == "":
-        print("main.py: " + cam + " source not found")
+        print("main.py: " + target + " source not found")
         return
-    file_out = os.path.basename(source + ".txt")
+    file_out = os.path.basename(target + ".txt")
     counter = dict()
     model = YOLO(ai_dir + "/models/yolo11x.pt")
     predictions = model.predict(
         source=source, classes=classes, conf=conf, device=device
     )
     for result in predictions:
-        img_shape = result.orig_shape
         if zone is None:
-            zone = [(0, 0), (0, img_shape[1]), img_shape, (img_shape[0], 0)]
+            zone = [(0, 0), (0, 1), (1, 1), (1, 0)]
         polygon = Polygon(zone)
         # result.show()
         for box in result.boxes:
@@ -76,8 +74,8 @@ def predict(cam, classes=[], conf=conf, device="cpu"):
             if class_name not in counter:
                 counter[class_name] = 0
             centroid = Point(
-                (float(box.xyxyn[0][2]) + float(box.xyxyn[0][0])) / 2 * img_shape[0],
-                (float(box.xyxyn[0][3]) + float(box.xyxyn[0][1])) / 2 * img_shape[1],
+                (float(box.xyxyn[0][0]) + float(box.xyxyn[0][2])) / 2,
+                (float(box.xyxyn[0][1]) + float(box.xyxyn[0][3])) / 2,
             )
             if polygon.contains(centroid):
                 counter[class_name] += 1
